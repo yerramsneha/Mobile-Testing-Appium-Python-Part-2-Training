7175457568d617e8a0b592a11757bedaa67c14d0
sivakarthikunnikrishnan <71559111+sivakarthikunnikrishnan@users.noreply.github.com>
2022-05-30 19:16:55 +0530

    Add files via upload

---
+++ calculator2Test.py
@@ -0,0 +1,22 @@
+from appium import webdriver
+import time
+
+from appium.webdriver.common.touch_action import TouchAction
+
+desired_cap = {"appium:deviceName": "Android Emulator",
+  "platformName": "Android",
+  "appium:appPackage": "com.android2.calculator3",
+  "appium:appWaitActivity": "com.xlythe.calculator.material.Theme.Orange",
+  "appium:app": "D:\\Downloads\\Calculator.apk"}
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_cap)
+driver.find_element_by_id("com.android2.calculator3:id/formula").send_keys("2")
+driver.find_element_by_id("com.android2.calculator3:id/op_add").click()
+driver.find_element_by_id("com.android2.calculator3:id/formula").send_keys("5")
+driver.find_element_by_id("com.android2.calculator3:id/eq").click()
+s = time.strftime("%Y_%m_%d_%H%M%S")
+activityname = driver.current_activity
+filename = activityname + s
+driver.save_screenshot("/Users/sivak/PycharmProjects/pythonCalculator2/screenshot/" + filename + ".png")
+
+
